6a200aa596ee20e710356cc3ce40ad0d00ebf43c
adaltekos <69512624+adaltekos@users.noreply.github.com>
2023-04-19 10:44:18 +0200

    Add files via upload

---
+++ PasswordExpirationNotification.ps1
@@ -0,0 +1,70 @@
+﻿#Install-Module CredentialManager
+#Install-Module ActiveDirectory
+
+$cred = Get-StoredCredential -Target '' #complete with name of stored credential in credential manager
+$textEncoding = [System.Text.Encoding]::UTF8
+
+#Get users from AD that are Enabled, have disabled option PasswordNeverExpires and Password is not Expired
+$users = get-aduser -filter * -properties Name, PasswordNeverExpires, PasswordExpired, PasswordLastSet, EmailAddress | where {$_.Enabled -eq "True"} | where { $_.PasswordNeverExpires -eq $false } | where { $_.passwordexpired -eq $false }
+#Get the Domain Password Policy for users that we are not able to read password policy
+$DefaultmaxPasswordAge = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge
+
+foreach ($user in $users)
+{
+    $Name = $user.Name
+    $emailaddress = $user.emailaddress
+    $passwordSetDate = $user.PasswordLastSet
+    $PasswordPol = (Get-AduserResultantPasswordPolicy $user)
+    if (($PasswordPol) -ne $null) {$maxPasswordAge = ($PasswordPol).MaxPasswordAge}
+	else {$maxPasswordAge = $DefaultmaxPasswordAge}
+
+#Days To Expire
+    $expireson = $passwordsetdate + $maxPasswordAge
+    $today = (get-date)
+    $daystoexpire = (New-TimeSpan -Start $today -End $Expireson).Days
+
+#Message
+    if (($daystoexpire) -gt "1")
+    {
+        $messageDays1 = "wygaśnie za " + "<b>$daystoexpire</b>" + " dni."
+		$messageDays2 = "wygaśnie za " + "$daystoexpire" + " dni."
+    }
+	else
+    {
+        $messageDays1 = "wygasa w dniu <b>DZISIEJSZYM</b>."
+		$messageDays2 = "wygasa w dniu DZISIEJSZYM."
+    }
+
+    $subject="Twoje hasło $messageDays2"
+    $body ="
+    Cześć $name.
+    <p> Wygląda na to że Twoje hasło domenowe $messageDays1<br><br>
+    By zmienić Twoje hasło do Windowsa (komputera), najpierw połącz się z siecią firmową<br>
+	&emsp;&emsp;Możesz to zrobić:<br>
+	<i>&emsp;&emsp;&emsp;&emsp;a) w firmie po kablu sieciowym lub przez sieć WiFi Border</i><br>
+	<i>&emsp;&emsp;&emsp;&emsp;b) poza firmą po zestawieniu VPNa</i><br>
+	po połączeniu nacisnij <b>CTRL</b>+<b>ALT</b>+<b>Delete</b> i wybierz Zmień Hasło <br><br>
+	<p>Przypominam, że połączenie VPN jak i dostęp do zasobów (np. INFOMEX) czy dostep do panelu Helpdesk jest skorelowane z Twoim kontem Windows. Oznacza to, że dane do autoryzacji w tych miejscach są pobierane z danych Twojego konta Windows.<br> 
+	Dlatego tak ważne jest zmienienie hasła przed jego wygaśnięciem. Nie zmienienie hasła przed jego wygaśnięciem zablokuje dostęp do wyżej wymienionych usług. 
+    <p>Pozdrawiam, Helpdesk IT<br>
+    </P>"
+
+    if (($daystoexpire -ge "0") -and (($daystoexpire -eq "1") -or ($daystoexpire -eq "3") -or ($daystoexpire -eq "7")))
+    {
+		$mailParams = @{
+			SmtpServer		= 'smtp.office365.com' #complete with smtp server
+			Port			= '587' # or '25' if not using TLS
+			UseSSL			= $true # or not if using non-TLS
+			Credential		= $cred
+			From			= '' #complete with from address
+			To				= $emailaddress
+			Subject			= $subject
+			Body			= $body
+			Priority		= 'High'
+			bodyasHTML		= $true
+		}
+		
+		Send-Mailmessage @mailParams -Encoding $textEncoding
+		
+    }
+}
